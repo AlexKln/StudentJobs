10c8652c37634b16e8da166e695af5a348b472e7
Alex <alexpmfm@gmail.com>
2019-09-03 23:29:59 +0300

    removed build from .gitignore

---
+++ frontend/build/precache-manifest.3a6a8872b0dd7f33b2ef6c78265cfa8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0799b5339ec636ae834c610d866ba50e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64e3340c4563ddeec04b",
+    "url": "/static/css/main.5767fb79.chunk.css"
+  },
+  {
+    "revision": "d586bde8a36eb582738f",
+    "url": "/static/js/2.aac0d7b7.chunk.js"
+  },
+  {
+    "revision": "64e3340c4563ddeec04b",
+    "url": "/static/js/main.5b8a045d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
