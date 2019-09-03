f9beba02e12e1333aa036c086f67bf2238bbff90
Alex <alexpmfm@gmail.com>
2019-09-03 22:45:24 +0300

    docker port fix

--- backend/api.py
+++ backend/api.py
@@ -28,4 +28,4 @@ api.add_resource(resources.UserLogin, '/login')
 api.add_resource(resources.ValidateToken, '/validate')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
