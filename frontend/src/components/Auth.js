3f551ecd8f3ae9078ae25bafeedbddcdda166797
Roy <roymeplease@gmail.com>
2020-04-11 19:54:38 +0300

    Auth.js - refactor to functional component
    Changed to functional component in order to use React Hooks.

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,70 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
 import { Link, Redirect } from 'react-router-dom';
 import Refresh from './Refresh';
 import NavigationBar from './NavigationBar';
 import axios from 'axios';
 import { Alert, Button, FormGroup, FormLabel, FormControl, Nav } from 'react-bootstrap'
 
-class Auth extends Component {
-    state = {
-        user: "",
-        password: "",
-        isLoggedIn: false,
-        successAlert: false
-    }
+function Auth(props) {
+    const [user, setUser] = useState("")
+    const [password, setPassword] = useState("")
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [successAlert, setSuccessAlert] = useState(false)
 
-    componentDidMount() {
-        const AuthStr = 'Bearer ' + this.getToken()
+    useEffect(() => {
+        const AuthStr = 'Bearer ' + getToken()
         axios.get('http://' + process.env.REACT_APP_HOST_URL + '/validate',
             { 'headers': { 'Authorization': AuthStr } })
-            .then(res => this.setState({ isLoggedIn: true })
-            )
-    }
+            .then(res => setLoggedIn(true))
+    }, [])
 
-    componentDidUpdate(prevProps, prevState) {
-        if (this.state.successAlert) {
-            this.turnOffSuccessAlert = setTimeout(() => {
-                this.setState({ successAlert: false })
+    useEffect(() => {
+        if (successAlert) {
+            setTimeout(() => {
+                setSuccessAlert(false)
             }, 1500)
         }
-    }
+    }, [successAlert])
 
-    componentWillUnmount() {
-        clearTimeout(this.turnOffSuccessAlert);
-        clearTimeout(this.turnOffFailureAlert);
-    }
-
-    setToken = idToken => {
+    function setToken(idToken) {
         // Saves user token to localStorage
         localStorage.setItem("id_token", idToken)
     }
 
-    getToken = () => {
+    function getToken() {
         // Retrieves the user token from localStorage
         return localStorage.getItem("id_token")
     }
 
-    handleChange = (event) => {
-        const { name, value } = event.target
-        this.setState({ [name]: value })
-    }
-
-    handleSubmit = (event) => {
+    function handleSubmit(event) {
         event.preventDefault();
-
         axios.post('http://' + process.env.REACT_APP_HOST_URL + '/login', {
-            username: this.state.user,
-            password: this.state.password
+            username: user,
+            password: password
         })
             .then(res => {
-                this.setToken(res.data.access_token)
-                this.setState(prevState => {
-                    prevState.successAlert = true
-                    prevState.isLoggedIn = true
-                    return prevState
-                }
-                )
+                setToken(res.data.access_token)
+                setSuccessAlert(true)
+                setLoggedIn(true)
             }
+
             ).catch(
                 error => {
                     try {
@@ -75,91 +58,70 @@ class Auth extends Component {
                     }
                 }
             )
-        // .catch(function (error) {
-        //     error.response &&
-        //         console.log(error.response.status)
-        // })
-        setTimeout(() => !this.state.isLoggedIn && this.setState({ failureAlert: true }), 100)
+        setTimeout(() => !loggedIn, 100)
     }
 
-    logOut = (event) => {
+    function logOut(event) {
         localStorage.removeItem("id_token")
-        this.setState({
-            user: "",
-            password: "",
-            isLoggedIn: false
-        })
+        setUser("")
+        setPassword("")
+        setLoggedIn(false)
     }
 
-    render() {
-        const loggedIn = () => {
-            return (
-                <React.Fragment>
-                    <NavigationBar 
-                    jobs={this.props.jobs}
-                    logOut={this.logOut}
-                    />
-                    {this.state.successAlert && <Alert
-                        key={1}
-                        variant="success"
+    return (<React.Fragment>
+        {loggedIn ? <React.Fragment>
+            <NavigationBar
+                jobs={props.jobs}
+                logOut={logOut}
+            />
+            {successAlert ? <Alert
+                key={1}
+                variant="success"
+            >
+                Success!
+                    </Alert> : null}
+            <p />
+            <Refresh refresh={props.refresh} />
+            <Redirect
+                to={{
+                    pathname: "/jobs",
+                    state: { jobs: props.jobs }
+                }}
+            />
+        </React.Fragment> : <div className="Login">
+                <form onSubmit={handleSubmit}>
+                    <FormGroup controlId="usedr" bssize="large">
+                        <FormLabel>Username</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="text"
+                            name="user"
+                            value={user}
+                            onChange={(e) => setUser(e.target.value)}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="password" bssize="large">
+                        <FormLabel>Password</FormLabel>
+                        <FormControl
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="text"
+                            name="password"
+                        />
+                    </FormGroup>
+                    <Button
+                        block
+                        bsize="large"
+                        type="submit"
                     >
-                        Success!
-                    </Alert>}
-                    <p />
-                    <Refresh refresh={this.props.refresh} />
-                    <Redirect
-                        to={{
-                            pathname: "/jobs",
-                            state: { jobs: this.props.jobs }
-                        }}
-                    />
-                </React.Fragment>
-            )
-        }
-
-        const loggedOut = () => {
-            return (
-                <div className="Login">
-                    <form onSubmit={this.handleSubmit}>
-                        <FormGroup controlId="usedr" bssize="large">
-                            <FormLabel>Username</FormLabel>
-                            <FormControl
-                                autoFocus
-                                type="text"
-                                name="user"
-                                value={this.state.user}
-                                onChange={this.handleChange}
-                            />
-                        </FormGroup>
-                        <FormGroup controlId="password" bssize="large">
-                            <FormLabel>Password</FormLabel>
-                            <FormControl
-                                value={this.state.password}
-                                onChange={this.handleChange}
-                                type="text"
-                                name="password"
-                            />
-                        </FormGroup>
-                        <Button
-                            block
-                            bsize="large"
-                            type="submit"
-                        >
-                            Login
+                        Login
                         </Button>
-                        <Nav.Link as={Link} to='/register'>
-                            Register
+                    <Nav.Link as={Link} to='/register'>
+                        Register
                         </Nav.Link>
-                    </form>
-                    <Redirect to='/' />
-                </div>
-            )
-        }
-
-        return (
-            this.state.isLoggedIn ? loggedIn() : loggedOut()
-        )
-    }
+                </form>
+                <Redirect to='/' />
+            </div>}
+    </React.Fragment>)
 }
-
 export default Auth;
